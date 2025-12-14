24c2ea46d9241f4b43369f5e37ed2a7ffea95b35
Pedro Henrique Ferreira <pedrohfdias.15122007@gmail.com>
2025-12-13 23:32:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const path = require("path");
 
 const app = express();
 
-// serve arquivos estáticos
-app.use(express.static(path.join(__dirname, "public")));
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
 
-// rota principal
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
