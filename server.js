aa06637d9f87b7ab814ee130f7eb1adb38088d09
Pedro Henrique Ferreira <pedrohfdias.15122007@gmail.com>
2025-12-13 23:30:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static("public"));
+// serve arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// rota principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Servidor rodando");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor rodando na porta", PORT);
 });
